dcde09ad77302547e282f58d386fecf0b12cdf5e
Kyle Krick <krkrick@gmail.com>
2023-01-31 21:55:15 -0800

    Added block.c

---
+++ chapter2/block.c
@@ -0,0 +1,15 @@
+/* Includes */
+#include <stdio.h>
+
+void main (void)
+{
+    int i = 3;
+
+    {
+        int i = 5;
+
+        printf("I in the inner block is %d.\n", i);
+    }
+        printf("I in the function block is %d.\n", i);
+
+}
