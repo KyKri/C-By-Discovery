700c82512d4cc4dbb5d4a5dcd98c321713dc733f
Kyle Krick <krkrick@gmail.com>
2023-02-01 07:58:23 -0800

    Added block2.c

---
+++ chapter2/block2.c
@@ -0,0 +1,17 @@
+/*
+ * This program illustrates the effects of variable hiding.
+ * In particular, this program will not compile due to i not being defined
+ * in the main function block. 
+*/
+/* Inlcude File */
+#include <stdio.h>
+
+void main (void)
+{
+  {
+    int i;
+    scanf("%d", &i);
+  }
+
+  printf("i is %d", i);
+}
